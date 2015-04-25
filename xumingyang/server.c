c4f8e37630bda3306c3641c7b4544d23007dc6cb
zhou-dong <82224165@qq.com>
2015-04-25 17:39:21 -0500

    add sht

--- xumingyang/server.c
+++ xumingyang/server.c
@@ -29,90 +29,109 @@
 #define CHECK_SIZE 2
 
 struct message {
-    int socket_connection;
+    int socket_fd;
 } ;
 
-int main(int argc, char *argv[]) {
+void error(const char *msg) ;
+void run_thread(struct message *m) ;
+int handler_request(int socket_connection) ;
 
-    if (argc < 2) {
+int main(int argc, char *argv[]) 
+{
+
+    if (argc < 2) 
+    {
         fprintf(stderr,"pls send port provided\n");
         exit(1);
     }
 
-    void error(const char *msg) ;
-    void thread_execute(struct message *m) ;
-    int connection(int socket_connection) ; 
-    struct sockaddr_in serve_address, client_address ;
-    socklen_t client_length ;
-    int socket_connection ;
-    int port_number = atoi(argv[1]);;
+    int port_no = atoi(argv[1]) ;
+    struct sockaddr_in serve_address ;
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    socket_connection = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket_connection < 0)
-        error("open socket faild");
-
+    if(socket_fd < 0)
+    {
+        error("sorry error of open socket");
+    }
     serve_address.sin_family = AF_INET;
     serve_address.sin_addr.s_addr = INADDR_ANY;
-    serve_address.sin_port = htons(port_number);
-    if (bind(socket_connection, (struct sockaddr *) &serve_address, sizeof(serve_address)) < 0)
-        error("fail on bind socket") ;
+    serve_address.sin_port = htons(port_no);
+    if (bind(socket_fd, (struct sockaddr *) &serve_address, sizeof(serve_address)) < 0)
+    {
+        error("sorry error on bind socket") ;
+    }
+    listen(socket_fd,5);
 
-    listen(socket_connection,5);
-    client_length = sizeof(client_address);
-
-    while (1) {
+    while (1) 
+    {
         pthread_t thread ;
-        struct message m = { .socket_connection = socket_connection } ;
-        pthread_create(&thread, NULL, (void *)&thread_execute,  (void *) &m);
+        struct message info = { .socket_fd = socket_fd } ;
+        pthread_create(&thread, NULL, (void *)&run_thread,  (void *) &info);
     }
 
-    close(socket_connection);
+    close(socket_fd);
     return 0;
 }
 
-void thread_execute(struct message *m){
+void run_thread(struct message *info)
+{
     struct sockaddr_in client_address ;
     socklen_t client_length ;
     client_length = sizeof(client_address);
-    int connection(int socket_connection) ;
     void error(const char *msg) ;
-    int new_socket_connection = accept(m->socket_connection, (struct sockaddr *) &client_address, &client_length);
-    if (new_socket_connection < 0)
-        error("faild on accept");
+    int new_socket_fd = accept(info->socket_fd, (struct sockaddr *) &client_address, &client_length);
+    if (new_socket_fd < 0)
+    {
+        error("error on accept new socket!");
+    }
     int pid = fork();
-    if (pid < 0)
-        error("fail on fork");
+    if (pid < 0) 
+    {
+        error("error on on fork");
+    }
     if (pid == 0) {
-        close(m->socket_connection);
-        connection(new_socket_connection);
+        close(info->socket_fd);
+        handler_request(new_socket_fd);
         exit(0);
-    } else
-        close(new_socket_connection) ;
+    }
+    else
+    {
+        close(new_socket_fd) ;
+    }
     pthread_exit(NULL);
 }
 
-int connection(int socket_connection) {
-    int get_request(int fd, char *buffer) ;
-    void send_error(int socket_connection) ;
-    void send_ok(int socket_connection) ;
-    void send_page(int socket_connection, FILE *fp); 
+int get_request(int fd, char *buffer) ;
+void no_found_header(int socket_connection) ;
+void ok_header(int socket_connection) ;
+void page_content(int socket_connection, FILE *fp); 
+
+int handler_request(int socket_connection) {
     char request[500], *ptr, result[1000];
     int file, length;
     if (get_request(socket_connection, request) == 0)
+    {
         printf("received failed\n");
+    }
     printf("%s\n", request);
     ptr = strstr(request, " HTTP/");
     if (ptr == NULL) 
+    {
         printf("Hey we git an not HTTP request!\n");
-    else {
+    }
+    else
+    {
         *ptr = 0;
         ptr = NULL;
         if (strncmp(request, "GET ", 4) == 0) 
+        {
             ptr = request + 4;
-        if (ptr[strlen(ptr) - 1] == '/'){
+        }
+        if (ptr[strlen(ptr) - 1] == '/')
+        {
             FILE *fp = fopen("index.html","r") ;
-            send_ok(socket_connection) ;
-            send_page(socket_connection, fp) ;
+            ok_header(socket_connection) ;
+            page_content(socket_connection, fp) ;
             return 1;
         }
         int len = strlen(ptr) ;
@@ -120,56 +139,71 @@ int connection(int socket_connection) {
         strncpy(tmp,&ptr[1],len-1) ;
         tmp[strlen(ptr)-1] = '\0' ;
         FILE *fp = fopen(tmp,"r") ;
-        if (fp==NULL) {
-            fp = fopen("404.html","r");
-            send_error(socket_connection);
-        } else
-            send_ok(socket_connection) ;
-        send_page(socket_connection, fp) ;
+        if (fp==NULL)
+        {
+            fp = fopen("no_page.html","r");
+            no_found_header(socket_connection);
+        }
+        else
+        {
+            ok_header(socket_connection) ;
+        }
+        page_content(socket_connection, fp) ;
     }
     return 1;
 }
 
-void send_ok(int socket_connection){
-    char *msg1 = "HTTP/1.1 200 OK\r\n" ;
-    char *msg2 = "Server : Web Server with C\r\n" ;
-    send(socket_connection, msg1, strlen(msg1), 0);
-    send(socket_connection, msg2, strlen(msg2), 0);
+void ok_header(int socket_fd)
+{
+    char *message_one = "HTTP/1.1 200 OK\r\n" ;
+    char *message_two = "Server : Web Server with C\r\n" ;
+    send(socket_fd, message_one, strlen(message_one), 0);
+    send(socket_fd, message_two, strlen(message_two), 0);
 }
 
-void send_error(int socket_connection){
-    char *msg1 = "HTTP/1.1 404 Not Found\r\n" ;
-    char *msg2 = "Server : Web Server with C\r\n" ;
-    send(socket_connection, msg1, strlen(msg1), 0);
-    send(socket_connection, msg2, strlen(msg2), 0);
+void no_found_header(int socket_fd)
+{
+    char *message_one = "HTTP/1.1 404 Not Found\r\n" ;
+    char *message_two = "Server : Web Server with C\r\n" ;
+    send(socket_fd, message_one, strlen(message_one), 0);
+    send(socket_fd, message_two, strlen(message_two), 0);
 }
 
-void send_page(int socket_connection, FILE *fp){
-    char result[1000] ;
-    fread(result,1,1000,fp);
-    send(socket_connection, result, strlen(result), 0); 
+void page_content(int socket_fd, FILE *fp)
+{
+    char content[1500] ;
+    fread(content,1,1500,fp);
+    send(socket_fd, content, strlen(content), 0); 
     fclose(fp);
     fp=NULL;
 }
 
-int get_request(int fd, char *buffer) {
+int get_request(int fd, char *buffer)
+{
     char *point = buffer ;
     int match_check = 0;
-    while (recv(fd, point, 1, 0) != 0){
-        if (*point == CHECK[match_check]){
+    while (recv(fd, point, 1, 0) != 0)
+    {
+        if (*point == CHECK[match_check])
+        {
             ++match_check ; 
-            if (match_check == CHECK_SIZE){
+            if (match_check == CHECK_SIZE)
+            {
                 *(point + 1 - CHECK_SIZE) = '\0';
                 return (strlen(buffer)) ;
             }
-        } else
+        } 
+        else
+        {
             match_check = 0 ;
+        }
         point ++ ;
     }
     return (0) ;
 }
 
-void error(const char *msg) {
+void error(const char *msg)
+{
     perror(msg);
     exit(1);
 }
