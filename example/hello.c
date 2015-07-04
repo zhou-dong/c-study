79cd26c7e85f56dad78a8d322f50b2c72c1897ba
zhou-dong <82224165@qq.com>
2015-07-04 03:36:55 -0500

    update c

--- example/hello.c
+++ example/hello.c
@@ -4,6 +4,16 @@
 int main()
 {
     printf("Hello world!\n");  
+
+    int i ;
+    for (i=0; i<10; i++)
+        printf("%d\n", i) ;
+
+    if (i==10)
+        printf("%s", "I equal 10");
+    else
+        printf("%d\n", i);
+
     system("pause");
     exit(0); 
 }
