b2cf2b131bb93fd7a810785c3116a9a234e88bba
zhou-dong <82224165@qq.com>
2015-08-07 18:33:11 -0500

    update

--- example/hello.cpp
+++ example/hello.cpp
@@ -1,4 +1,32 @@
-int main(){
+//类：就是包含了函数的structure
+#include <iostream>
+using namespace std;
 
+class Rectangle {
+
+    public: int w ; 
+           int h ;
+
+    void init(int w_, int h_){
+        w = w_;
+        h = h_ ;
+    }
+
+    int Area(){
+        return w * h ;
+    }
+
+    int Perimeter(){
+        return 2 * (w + h) ;
+    }
+
+} ;
+
+int main(){
+    
+    Rectangle r ;
+    r.init(10,20);
+    int a = r.Area() ;
+    cout << a << endl  ;
 
 }
