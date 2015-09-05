62848e9de48020ed9de16e6621c670aecdf2fdc7
zhou-dong <82224165@qq.com>
2015-09-04 22:19:45 -0500

    update

---
+++ example/kunkun01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+// 1. use command line the input a number
+
+// 2. Check the input number should be positive
+
+// 3. use fork() start a child process
+// implement the conjecutre
+
+// 4. main use wait() to waite child process
+
+// 5. exit the function 
+
+}
---
+++ example/kunkun02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+// 1. use command line the input a number
+
+// 2. Check the input number should be positive
+
+// 3. create shalled memory
+
+// 4. use fork() start a child process
+// implement the conjecutre and set result into shalled memory
+
+// 5. main use wait() to waite child process
+
+// 6. After child process input all the data into the shalled memory, main process output the data
+
+// 7. remove the data 
+
+// 8. exit the function 
+
+}
