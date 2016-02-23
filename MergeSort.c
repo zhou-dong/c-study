579e18c7491b9801fa852cdbe983393d69c7fe12
zhou-dong <82224165@qq.com>
2016-02-22 23:00:17 -0600

    update

--- MergeSort.c
+++ MergeSort.c
@@ -1,10 +1,12 @@
 /* C program for merge sort */
-#include<stdlib.h>
-#include<stdio.h>
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 /*
-add share memory 
+add share memory
 */
 
 
